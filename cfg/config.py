33448edadddc7bf05d2e0be60980a4f71ed5a26c
Nikita Sharudin <nikita.sharudin@mail.ru>
2025-08-07 19:53:59 +0300

    cfg init

---
+++ cfg/config.py
@@ -0,0 +1,17 @@
+CONNECTION_STRING = 'udp:127.0.0.1:14550'
+
+
+CAMERA_INDEX = 0
+CAMERA_MATRIX = {
+    'fx': 800.0, # Focal length
+    'fy': 800.0,
+    'cx': 320.0, # Optical center
+    'cy': 240.0,
+}
+
+
+TARGET_TAG_ID = 1
+TAG_SIZE_METERS = 0.16 # IRL size of the AprilTag (in meters)
+
+
+REFRESH_RATE_SECONDS = 0.1
