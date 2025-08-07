aee339c85d3d373fd42464a01cffb11a0b3f3f39
Nikita Sharudin <nikita.sharudin@mail.ru>
2025-08-07 19:53:30 +0300

    logger init

---
+++ internal/logger/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+def setupLogger():
+    logger = logging.getLogger('OpenDLLogger')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(handler)
+
+    return logger
